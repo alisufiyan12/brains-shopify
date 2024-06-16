9f034e5a698850a204e79973ec3681df781a5d91
Li-Grey <khubaib7236@gmail.com~>
2024-06-16 15:55:23 +0500

    first commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,13 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
+'use client'
+
+import React from 'react';
+import VideoThumb from '@/public/images/hero-image.png';
+import ModalVideo from '@/components/modal-video';
+import { FaPlay } from "react-icons/fa";
+import MuxPlayer from "@mux/mux-player-react"; 
+
+import ReactPlayer from 'react-player'
+
 
 export default function Hero() {
   return (
@@ -9,6 +17,8 @@ export default function Hero() {
       <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
           <defs>
+            {/* 
+          */}
             <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
               <stop stopColor="#FFF" offset="0%" />
               <stop stopColor="#EAEAEA" offset="77.402%" />
@@ -22,16 +32,16 @@ export default function Hero() {
         </svg>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 ">
 
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+          <div className="text-center pb-12 md:pb-16 ">
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Welcome to <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#000081] to-[#96bf48]">Shopify</span> Masterclass</h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Unlock Your Potential in E-commerce, Marketing, and Design – Discover How in the Video Below!</p>
+                {/* 
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
@@ -40,10 +50,11 @@ export default function Hero() {
                   <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
                 </div>
               </div>
+                */}
             </div>
           </div>
 
-          {/* Hero image */}
+          {/* Hero image  - this is react so its better to use Mux but be careful with 4k uploades and the cost
           <ModalVideo
             thumb={VideoThumb}
             thumbWidth={768}
@@ -52,6 +63,27 @@ export default function Hero() {
             video="/videos/video.mp4"
             videoWidth={1920}
             videoHeight={1080} />
+
+                <MuxPlayer
+                stream-type="on-demand"
+                playback-id="oXHtoOkO1BnlomhrOMheB9oWEsTmVXE229HX02J1Vbw4"
+                metadata-video-title="Tea and Cookies"
+                metadata-viewer-user-id="user-24601"
+                accent-color=''
+                ></MuxPlayer>
+          <div className='flex items-center justify-center border-red-400 border-2'>
+          */}
+          <div id="headd" className="h-64 sm:h-[630px] flex justify-center rounded-md overflow-hidden m-0 p-0 shadow-md">
+          <ReactPlayer 
+          className=""
+          width='100%' 
+          height='100%'
+          controls={true}
+          url='https://youtu.be/s4adrGFa_6A' />
+
+          </div>
+
+
 
         </div>
 
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
         <div className="flex items-center justify-between h-16 md:h-20">
 
           {/* Site branding */}
-          <div className="shrink-0 mr-4">
+          <div className="shrink-0 mr-4 m-6">
             <Logo />
           </div>
 
@@ -36,12 +36,10 @@ export default function Header() {
           <nav className="hidden md:flex md:grow">
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
+              
               <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
-              </li>
-              <li>
-                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
+                <Link href="#headd" className="btn-sm duration-300 transition ease-in-out hover:bg-blue-600  text-gray-200 bg-gray-900 ml-3">
+                  <span>Watch video</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
